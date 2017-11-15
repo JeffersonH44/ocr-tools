83519743a6a5caeb9d41a74f9e2480f6bae21736
Jefferson Javier Hernández Panqueba <jeffersonh44@gmail.com>
2017-11-15 11:26:13 -0500

    update mrz using roi size

--- run/getMRZdata.js
+++ run/getMRZdata.js
@@ -21,11 +21,50 @@ const maskOptions = {
     invert: true,
     algorithm: 'isodata'
 };
+const maxSizeRoi = 800;
+const filterSize = 0.86;
 
 var files = fs.readdirSync(readPath);
+console.log(files);
 var promises = files.map(elem => IJS.load(readPath + elem));
 var table = [];
 var checkRoi = number => 15 <= number && number <= 50;
+
+function similarityPeaks(peaks) {
+    if(peaks.length < 2) {
+        throw new RangeError(`similarityPeaks expects to receive an array greater than two, received an array of length; ${peaks.length}`);
+    }
+
+    var sim = new Array(peaks.length - 1);
+    for(var i = 0; i < peaks.length - 1; ++i) {
+        var peak1 = peaks[i];
+        var peak2 = peaks[i + 1];
+
+        sim[i] = tanimoto([peak1.end - peak1.start], [peak2.end - peak2.start]);
+    }
+
+    return sim;
+}
+
+function similarityBetweenPeaks(peaks) {
+    if(peaks.length < 3) {
+        throw new RangeError(`similarityBetweenPeaks expects to receive an array greater than 3, received an array of length; ${peaks.length}`);
+    }
+
+    var sim = new Array(peaks.length - 2);
+    for(var i = 0; i < peaks.length - 2; ++i) {
+        var peak1 = peaks[i];
+        var peak2 = peaks[i + 1];
+        var peak3 = peaks[i + 2];
+
+        var info1 = getInfo(peak2, peak1);
+        var info2 = getInfo(peak3, peak2);
+
+        sim[i] = tanimoto(info1, info2);
+    }
+
+    return sim;
+}
 
 function parseInfo(info, roiIds, check) {
     var size = info.length;
@@ -45,12 +84,14 @@ function parseInfo(info, roiIds, check) {
 }
 
 function filterManager(manager) {
-    var roiIds = manager.getRoiIds(roiOptions);
+    var rois = manager.getRois(roiOptions);
     var rowsInfo = manager.getMap().rowsInfo();
     //var colsInfo = manager.getMap().colsInfo();
 
     return {
-        rowInfo: parseInfo(rowsInfo, roiIds, checkRoi),
+        parseRowInfo: parseInfo(rowsInfo, rois.map(elem => elem.id), checkRoi),
+        rowsInfo: rowsInfo,
+        rois: rois
         //colsInfo: parseInfo(colsInfo, roiIds, checkRoi),
     };
 }
@@ -59,9 +100,24 @@ function getInfo(peakA, peakB) {
     return [Math.abs(peakA.end - peakB.start), peakA.end - peakA.start, peakB.end - peakB.start];
 }
 
-function getMRZ(medianHistogram) {
+function transformROIs(rois) {
+    var output = {};
+    for (var i = 0; i < rois.length; ++i) {
+        var roiId = rois[i].id;
+        output[roiId] = rois[i];
+    }
+
+    return output;
+}
+
+function getMRZ(medianHistogram, rowsInfo, rois, imageWidth) {
+    console.log('start getMRZ');
+    var rois = transformROIs(rois);
+
     var peaks = [];
     var start;
+
+    // get all median histogram by row
     for (var i = 0; i < medianHistogram.length; i++) {
         var element = medianHistogram[i];
         if (element !== 0 && start === undefined) {
@@ -71,40 +127,82 @@ function getMRZ(medianHistogram) {
             peaks.push({
                 start: start,
                 end: end,
-                middle: start + ((end - start) / 2)
+                middle: Math.round(start + ((end - start) / 2))
             });
             start = undefined;
         }
     }
 
-    peaks.reverse();
-    var peakEnd = peaks[0];
-    var peakStart;
-    var similarity;
-
-    if (peaks.length === 2) {
-        peakStart = peaks[1];
-    } else if (peaks.length > 2) {
-        var peak1 = peaks[0];
-        var peak2 = peaks[1];
-        var peak3 = peaks[2];
-        // because I reverse it
-        var info1 = getInfo(peak2, peak1);
-        var info2 = getInfo(peak3, peak2);
-        similarity = tanimoto(info1, info2)
-        if (similarity > 0.9) {
-            peakStart = peak3;
-        } else {
-            peakStart = peak2;
+    var filteredPeaks = [];
+    var filteredHistogram = new Array(medianHistogram.length).fill(0);
+    // check if the center of each peak contains ROI's greater that certain percentage
+    for (i = 0; i < peaks.length; i++) {
+        var minX = Number.MAX_VALUE;
+        var maxX = 0;
+        var middlePoint = peaks[i].middle;
+        var currentInfo = rowsInfo[middlePoint].positiveRoiIDs;
+        for (var j = 0; j < currentInfo.length; ++j) {
+            // TODO: why roisId return a string instead of number
+            var currentRoi = rois[parseInt(currentInfo[j])];
+            if (currentRoi && currentRoi.maxX - currentRoi.minX < maxSizeRoi) {
+                minX = Math.min(minX, currentRoi.minX);
+                maxX = Math.max(maxX, currentRoi.maxX);
+            }
         }
-    } else {
+
+        console.log('filter size:', (maxX - minX) / imageWidth);
+        if ((maxX - minX) / imageWidth > filterSize) {
+            var currentPeak = peaks[i];
+            filteredPeaks.push(currentPeak);
+            for (j = currentPeak.start; j < currentPeak.end; ++j) {
+                filteredHistogram[j] = medianHistogram[j];
+            }
+        }
+    }
+
+    peaks = filteredPeaks;
+    console.log('total peaks', peaks.length);
+
+    if(peaks.length === 0) {
         throw new RangeError('Not able to find the MRZ');
     }
+
+    peaks.reverse();
+
+    var peakStart = peaks[0];
+    var peakEnd = peaks[0];
+    var simPeaks = similarityPeaks(peaks);
+    var simBetweenPeaks = [];
+
+    if(peaks.length >= 2) {
+        if(simPeaks[0] > 0.8) {
+            peakEnd = peaks[1];
+        }
+
+        if (peaks.length > 2) {
+            simBetweenPeaks = similarityBetweenPeaks(peaks);
+            for (i = 0; i < simBetweenPeaks.length; ++i) {
+                if (simBetweenPeaks[i] > 0.88) {
+                    peakEnd = peaks[i + 2];
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    // we have to swap it because we apply a reverse over peaks variable
+    [peakStart, peakEnd] = [peakEnd, peakStart];
+
+    simPeaks.reverse();
+    simBetweenPeaks.reverse();
 
     return {
         y: peakStart.start,
         height: peakEnd.end - peakStart.start,
-        similarity
+        filteredHistogram,
+        simPeaks,
+        simBetweenPeaks
     };
 }
 
@@ -127,21 +225,29 @@ Promise.all(promises).then(function (images) {
         manager.fromMask(mask);
 
         var {
-            rowInfo,
-            //colsInfo
+            parseRowInfo,
+            rowsInfo,
+            rois
         } = filterManager(manager);
 
-        var {
-            y,
-            height,
-            similarity
-        } = getMRZ(rowInfo);
+        try {
+            var {
+                y,
+                height,
+                filteredHistogram,
+                simPeaks,
+                simBetweenPeaks
+            } = getMRZ(parseRowInfo, rowsInfo, rois, image.width);
+        } catch (e) {
+            console.log('not able to find mrz for', files[i]);
+            continue;
+        }
 
         var margin = 0;
-        
+
         var crop = image.crop({
             y: y - margin,
-            height: height + 2*margin
+            height: height + 2 * margin
         });
 
         if (!fs.existsSync(saveMRZ)) {
@@ -158,8 +264,10 @@ Promise.all(promises).then(function (images) {
                 `<img src="./${maskPath}" width="600" height="600">`,
                 `<img src="./${cropPath}" width="600" height="200">`,
             ],
-            'Row info median': `<span class='histogram'>${rowInfo.join(',')}</span>`,
-            'similarity': similarity
+            'Row info median': `<span class='histogram'>${parseRowInfo.join(',')}</span>`,
+            'Filtered info median': `<span class='histogram'>${filteredHistogram.join(',')}</span>`,
+            simPeaks: simPeaks,
+            simBetweenPeaks: simBetweenPeaks
             // 'Col info median': `<span class='histogram'>${colsInfo.join(',')}</span>`
         });
     }
